f02559f2a9ff70f2d95b4f0a2173a83eca3af43c
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-10-22 21:06:30 +0530

    [Sai]Added. Created Binary tree and Display

--- BinaryTree/Program.cs
+++ BinaryTree/Program.cs
@@ -7,6 +7,11 @@ namespace BinaryTree
         static void Main(string[] args)
         {
             Console.WriteLine("Binary Tree Implimentation!");
+
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            binarySearchTree.Display();
         }
     }
 }
